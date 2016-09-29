0cde84a73c2248711153b244ad9f8f8305d2bd35
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2016-09-29 15:31:04 +0900

    異常時に位置情報を取得するようにした

--- app/src/main/java/jp/realglobe/sugo/actor/hitoe/MainActivity.java
+++ app/src/main/java/jp/realglobe/sugo/actor/hitoe/MainActivity.java
@@ -1,21 +1,37 @@
 package jp.realglobe.sugo.actor.hitoe;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -41,6 +57,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Vibrator vibrator;
     private Ringtone ringtone;
+    private final int requestCode = (int) (Integer.MAX_VALUE * Math.random());
+
+    private GoogleApiClient googleApiClient;
+    private volatile Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +70,86 @@ public class MainActivity extends AppCompatActivity {
         this.timerHandler = new Handler();
         this.vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         this.ringtone = RingtoneManager.getRingtone(this, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
+        checkLocationPermission();
+
+        this.googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new MyLocationListener())
+                .addOnConnectionFailedListener(connectionResult -> Log.w(LOG_TAG, "Location detection error: " + connectionResult))
+                .build();
 
         reset();
 
         this.executor.submit(this::runForEvent);
         Log.d(LOG_TAG, "Event waiter started");
+    }
+
+    private void checkLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 位置情報には許可が必要。
+            this.requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            }, requestCode);
+        } else {
+            showLocationNotice(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != this.requestCode) {
+            return;
+        }
+
+        final Set<String> required = new HashSet<>(Arrays.asList(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION));
+        for (int i = 0; i < permissions.length; i++) {
+            if (!required.contains(permissions[i])) {
+                continue;
+            }
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                continue;
+            }
+            required.remove(permissions[i]);
+        }
+
+        showLocationNotice(required.isEmpty());
+    }
+
+    private void showLocationNotice(boolean allowed) {
+        final String message;
+        if (allowed) {
+            message = "救助要請に位置情報を付加できます";
+        } else {
+            message = "救助要請に位置情報を付加することができません\nメニューから許可設定を行ってください";
+        }
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private class MyLocationListener implements ConnectionCallbacks, LocationListener {
+
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            final Location location = LocationServices.FusedLocationApi.getLastLocation(MainActivity.this.googleApiClient);
+            MainActivity.this.location = location;
+
+            Log.d(LOG_TAG, "Location is " + location);
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            MainActivity.this.location = location;
+
+            Log.d(LOG_TAG, "Location changed to " + location);
+        }
     }
 
     @Override
@@ -96,6 +191,8 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
+        } else if (item.getItemId() == R.id.action_allow) {
+            checkLocationPermission();
         } else if (item.getItemId() == R.id.action_reset) {
             reset();
         } else if (item.getItemId() == R.id.action_call) {
@@ -120,6 +217,7 @@ public class MainActivity extends AppCompatActivity {
         this.eventQueue.clear();
         this.vibrator.cancel();
         this.ringtone.stop();
+        this.googleApiClient.disconnect();
 
         Log.d(LOG_TAG, "Mode was reset");
     }
@@ -168,6 +266,8 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .show());
 
+        this.googleApiClient.connect();
+
         Log.d(LOG_TAG, "Warning mode started");
     }
 
@@ -188,6 +288,9 @@ public class MainActivity extends AppCompatActivity {
         this.eventQueue.clear();
         this.vibrator.cancel();
         this.ringtone.stop();
+        if (!(this.googleApiClient.isConnecting() && this.googleApiClient.isConnected())) {
+            this.googleApiClient.connect();
+        }
 
         Log.d(LOG_TAG, "Emergency mode started");
     }
